57226e999d206faf431d2aa3bf35a9b11df8000d
tcwood <travis.cwood@gmail.com>
2016-09-14 14:55:37 -0700

    Implement stack (functional)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,27 @@
 var Stack = function() {
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function() {
+    var result = storage[counter];
+    if (counter > 0) {
+      counter--;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
 };
+
