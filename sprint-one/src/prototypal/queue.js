f252c958e2ee1acc8fc1fe9cf34cd0ce02f667d2
tcwood <travis.cwood@gmail.com>
2016-09-14 16:35:56 -0700

    Implement stack and queue (prototypal)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.counter = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+
+  dequeue: function() {
+    var result = this.storage[1];
+    var key = 1;
+    var newStorage = {};
+
+    if (this.counter > 0) {
+      for (var item in this.storage) {
+        if (item !== '1') {
+          newStorage[key] = this.storage[item];
+          key++;
+        }
+      }
+      this.storage = newStorage;
+      this.counter--;
+    }
+    return result;
+  },
+
+  size: function() {
+    return this.counter;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.counter = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+  
+  pop: function() {
+    var result = this.storage[this.counter];
+    if (this.counter > 0) {
+      delete this.storage[this.counter];
+      this.counter--;
+    }
+    return result;
+  },
+  
+  size: function() {
+    return this.counter;
+  }
+};
 
 
