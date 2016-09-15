844eb004e7cad78119caf84f22b95aa017a51083
tcwood <travis.cwood@gmail.com>
2016-09-14 17:52:00 -0700

    Implement stack and queue (pseudoclassical)

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
-var Stack = function() {
+var Stack = function(counter, storage) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.counter = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.counter++;
+  this.storage[this.counter] = value;
+};
 
+Stack.prototype.pop = function() {
+  var result = this.storage[this.counter];
+  if (this.counter > 0) {
+    delete this.storage[this.counter];
+    this.counter--;
+  }
+  return result;  
+};
+
+Stack.prototype.size = function() {
+  return this.counter;
+};
