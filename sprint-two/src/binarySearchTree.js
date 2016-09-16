71f5d49daad791387001b2c3520b9f0ed94f87a4
tcwood <travis.cwood@gmail.com>
2016-09-15 20:20:18 -0700

    Start BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,38 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  return tree;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var child = BinarySearchTree(value);
+  var noChildren = child.left === null && child.right === null;
+  var twoChildren = child.left !== null && child.right !== null;
+
+  var numChildren = noChildren ? 0 : twoChildren ? 2 : 1;
+
+
+  if (this.tree.value < value) {
+    this.tree.right = child;  
+
+
+    // check left or right
+    //   attach child to whichever
+  } 
+
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
 };
 
 
